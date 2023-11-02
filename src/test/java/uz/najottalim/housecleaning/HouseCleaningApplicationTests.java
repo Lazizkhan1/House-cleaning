8304a1278ed20e22060151300d800428a8e816a8
Lazizkhan1 <lazizkhanhadjayev@gmail.com>
2023-11-01 21:16:06 -0700

    Inital commit

---
+++ src/test/java/uz/najottalim/housecleaning/HouseCleaningApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.najottalim.housecleaning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseCleaningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
